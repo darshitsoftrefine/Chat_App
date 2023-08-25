7997a803675e6261e4c6cd88414568fe23392c4c
Darshit Shah <darshit.softrefine@gmail.com>
2023-08-25 10:50:30 +0530

    Phone Authentication Done

--- lib/screens/auth_screen/login_screen.dart
+++ lib/screens/auth_screen/login_screen.dart
@@ -2,10 +2,8 @@ import 'package:chat_app/constants/custom_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import '../../verify_code.dart';
-
 class LoginScreen extends StatefulWidget {
-  LoginScreen({super.key});
+  const LoginScreen({super.key});
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
@@ -15,61 +13,76 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController phoneController = TextEditingController();
 
   TextEditingController otpController = TextEditingController();
-  bool otp = false;
+  bool otpCodeVisible = false;
 
   FirebaseAuth auth = FirebaseAuth.instance;
 
-  String verificationID = "";
+  String verificationIDReceived = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.teal,
-        title: Text("Login Page"),
-        actions: [
-        ],
+        title: const Text("Login Page"),
       ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.only(top: 20, left: 34, right: 34, bottom: 10),
           child: Column(
             children: [
-              Text("Login", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),),
-              SizedBox(height: 20,),
+              const Text("Login", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),),
+              const SizedBox(height: 20,),
               //Expanded(child: SizedBox()),
               CustomField(label: 'Phone Number', control: phoneController, obs: false, hint: 'Enter Phone Number'),
-              SizedBox(height: 20,),
+              const SizedBox(height: 20,),
               Visibility(
-                visible: otp,
+                visible: otpCodeVisible,
                   child: CustomField(label: "OTP", control: otpController, obs: false, hint: "Enter OTP")),
               ElevatedButton(onPressed: (){
-                auth.verifyPhoneNumber(
-                  phoneNumber: phoneController.text,
-                    verificationCompleted: (PhoneAuthCredential credential) async{
-                      await auth.signInWithCredential(credential).then((value) => {
-                        print("Successfully")
-                      });
-                    },
-                    verificationFailed: (e){
-                    print(e);
-                    },
-                    codeSent: (String verification, int? token){
-
-                    verificationID =  verification;
-                    otp = true;
-                    setState(() {
-
-                    });
-                    },
-                    codeAutoRetrievalTimeout: (String verification){
-                    print(verification);
-                    });
-              }, child: Text(otp ? "Login":"Verify"))
+                if(otpCodeVisible){
+                  verifyCode();
+                } else {
+                  verifyNumber();
+                }
+                verifyNumber();
+              }, child: Text(otpCodeVisible ? "Login":"Verify"))
             ],
           ),
         ),
       ),
     );
+  }
+  void verifyNumber(){
+    auth.verifyPhoneNumber(
+        phoneNumber: phoneController.text,
+        verificationCompleted: (PhoneAuthCredential credential) async{
+          await auth.signInWithCredential(credential).then((value) {
+            debugPrint("Successfully");
+          });
+        },
+        verificationFailed: (FirebaseAuthException exception){
+          debugPrint("Hello Failed ${exception.message}");
+        },
+        codeSent: (String verificationID, int? token){
+          //PhoneAuthCredential credential = PhoneAuthProvider.credential(verificationId: verificationID, smsCode: otpController.text);
+          verificationIDReceived =  verificationID;
+          otpCodeVisible = true;
+          setState(() {
+
+          });
+        },
+        codeAutoRetrievalTimeout: (String verificationID){
+          debugPrint("ID Received $verificationID");
+        });
+  }
+  void verifyCode() async{
+    PhoneAuthCredential credential = PhoneAuthProvider.credential(
+        verificationId: verificationIDReceived,
+        smsCode: otpController.text);
+
+    await auth.signInWithCredential(credential).then((value){
+      debugPrint("Logged In Successfully");
+    });
   }
 }
